b85745beace132bc98bead0e9d93e07e1a9fc381
AbeTavarez <abeincwireless@gmail.com>
2024-12-02 23:13:30 -0500

    summarize ai tool

--- streamlit-demos/ai-writing-assistant/app.py
+++ streamlit-demos/ai-writing-assistant/app.py
@@ -9,9 +9,6 @@ st.set_page_config(page_title="AI Writing Tool")
 
 st.title("AI Writing Tool")
 
-# Text
-text_to_summarize = st.text_area('Enter text to summarize')
-
 # LLM (Model)
 llm = ChatGroq(groq_api_key=st.secrets["GROQ_API_KEY"], model="llama3-8b-8192")
 
@@ -24,10 +21,20 @@ parser = StrOutputParser()
 # Chain
 chain = prompt_template | llm | parser 
 
+st.session_state["summarized_text"] = ""
+
 with st.form("summarize_form", clear_on_submit=True):
+  # Text
+  text_to_summarize = st.text_area('Enter text to summarize')
   submitted = st.form_submit_button('Submit')
 
   if submitted:
+    st.session_state["summarized_text"] = ""
     with st.spinner('Thinking...'):
       result = chain.invoke({"text": text_to_summarize})
       print(result)
+
+      st.session_state.summarized_text = result
+
+if st.session_state.summarized_text:
+  st.markdown(st.session_state.summarized_text)
