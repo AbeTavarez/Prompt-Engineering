67a20c2d477244a2cf0c4436a5a718a6909873e7
AbeTavarez <abeincwireless@gmail.com>
2024-12-05 17:22:42 -0500

    agent

---
+++ streamlit-demos/assistant-agent/app.v1.py
@@ -0,0 +1,55 @@
+import streamlit as st
+from langchain_groq import ChatGroq 
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.tools.tavily_search import TavilySearchResults
+
+
+# Model and Agent tools
+llm =  ChatGroq(api_key=st.secrets.get("GROQ_API_KEY"))
+search = TavilySearchResults(max_results=2)
+parser = StrOutputParser()
+# tools = [search] # add tools to the list
+
+# Page Header
+st.title("Assistant Agent")
+st.markdown("Assistant Agent Powered by Groq.")
+
+
+# Data collection/inputs
+product_name = st.text_input("Product Name:")
+company_url = st.text_input("Company URL:")
+competitors = st.text_input("Competitors list (ex. Apple, Tesla):")
+
+# Data process
+if st.button("Generate Insights"):
+  if product_name and company_url:
+    st.spinner("Processing...")
+
+    # search internet
+    company_data = search.invoke(company_url)
+    print(company_data)
+
+    prompt = """
+    You are a helpful AI assisting a sales representative. Your job is to analyze the provided company data, the product being sold, and the competitors to generate actionable insights. Provide:
+
+    1. A summary of the company's strategy.
+    2. Potential competitors from the list or company overview.
+    3. Key leadership or decision-maker insights.
+
+    Input variables:
+    - {company_data}
+    - {product_name}
+    - {competitors}
+    """
+
+    # Prompt Template
+    prompt_template = ChatPromptTemplate([("system", prompt)])
+
+    # Chain
+    chain = prompt_template | llm | parser
+
+    # Result/Insights
+    result = chain.invoke({"company_data": company_data, "product_name": product_name, "competitors": competitors})
+
+    st.markdown(result)
